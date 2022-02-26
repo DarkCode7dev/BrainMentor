c01e6d0fa084c2c49991da7c95d613a2afe2bf53
DarkCode7dev <55891686+DarkCode7dev@users.noreply.github.com>
2022-02-27 00:18:48 +0530

    Create typescript.ts

---
+++ typescript.ts
@@ -0,0 +1,24 @@
+//Implicit object
+var obj ={
+    name:'Am',
+    city:"Del"
+}
+obj.name='d';
+console.log(obj)
+
+//Explicit object
+
+var obj2 :{ name:string,city:string}={ name:'Am',city:"Del"}
+console.log(obj2)
+
+var arr= [10,20,30]//Implicit array
+var arr2 : number[]=[10,20,30]//Explicit array
+
+var d: number|boolean;
+
+
+type myType={
+    id:number, name: string, salary:number
+}
+//x object of type myType
+var x : myType;
