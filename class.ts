0a64d008b7f45b84ed5894b230c4557b3fe9a9f9
DarkCode7dev <55891686+DarkCode7dev@users.noreply.github.com>
2022-03-12 01:01:47 +0530

    Update class.ts

--- class.ts
+++ class.ts
@@ -2,13 +2,33 @@ class Employee{
     private id:number;
     private name:string;
     private salary:number;
+    private _phone:string;
+    private _email:string;
     constructor(id:number,name:string,salary:number){
         this.id=id;
         this.name= name;
         this.salary=salary;
     }
 
+set phone(phone:string){
+this._phone=phone;
+} 
 
+get phone(){
+    return this._phone;
+}
+
+
+
+
+set email(email:string){
+this._email=email;
+} 
+
+
+get email(){
+    return this._phone;
+}
     public show():void{
             console.log(`Id is ${this.id} , ${this.name},${this.salary} `);
     }
@@ -16,3 +36,5 @@ class Employee{
 
 var ram :Employee =new Employee(12,"lo",58547887);
 ram.show();
+ram.email='f'
+
