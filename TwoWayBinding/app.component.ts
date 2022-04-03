0b3e7cc2619f778635fa3a612e81973155be831a
DarkCode7dev <55891686+DarkCode7dev@users.noreply.github.com>
2022-04-03 09:23:35 +0530

    Create app.component.ts

---
+++ TwoWayBinding/app.component.ts
@@ -0,0 +1,10 @@
+{{title}}   ==>example of string interpollation use this  
+
+<br>
+==================================================
+<p>This is example of Property Binding</p>
+<p [innerHtml]="title"> This is example of Property Binding</p> 
+<br>
+  
+  <input type="text" [(ngModel)]="myname">
+{{myname}}
